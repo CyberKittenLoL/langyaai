d252bf0b2cd0d5b58dbddd1fef5960526cf2540a
CyberkittenMEOW <kelvinngkelvinng@gmail.com>
2025-12-19 11:36:30 +0800

    test CDN

---
+++ test_app.js
@@ -0,0 +1,10 @@
+
+
+console.log("ABC");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const d = document.querySelector("div.abc");
+  if (d) d.style.backgroundColor = "#0000ff";
+});
+
+
