a14fd2c9f8964684623627af3534176c176edede
CyberkittenMEOW <kelvinngkelvinng@gmail.com>
2025-12-19 11:40:49 +0800

    Change to alert

--- test_app.js
+++ test_app.js
@@ -3,8 +3,7 @@
 console.log("ABC");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const d = document.querySelector("div.abc");
-  if (d) d.style.backgroundColor = "#0000ff";
+    alert("First version");
 });
 
 
